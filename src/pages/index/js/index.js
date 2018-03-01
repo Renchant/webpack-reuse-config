b34a5ef2490befc1b5b2e85e2c9248cd481d9ac2
奶爷 <you@example.com>
2018-03-01 10:04:30 +0800

    add

--- src/pages/index/js/index.js
+++ src/pages/index/js/index.js
@@ -1,21 +1,69 @@
 import '../style/index.scss';
 
 import bg from 'Static/images/bg.png';
+import { listData } from './module/data';
 import { h, app } from 'hyperapp';
 
+import { List } from './module/list';
+console.log(listData);
+
 const state = {
-    bg: bg
+    bg: bg,
+    list: listData,
+    free_count: 5924,
+    name: '',
+    phone: '',
+    code: ''
 };
 
 const actions = {
-    down: value => state => ({ count: state.count - value }),
-    up: value => state => ({ count: state.count + value })
-};
+    diagnosis: item => state => {
+        console.log(item);
+    },
+    nameInput: ({ value }) => ({
+        name: value
+    }),
+    phoneInput: ({ value }) => ({
+        phone: value
+    }),
+    codeInput: ({ value }) => ({
+        code: value
+    })
+}
+
 const view = (state, actions) => (
     <div class='container'>
         <header class='header'>
             <img src={state.bg} />
         </header>
+        <main class='main'>
+            <ul class='main-list'>
+                <List arr={state.list} event={actions.diagnosis} />
+            </ul>
+        </main>
+        <footer class='footer'>
+            <p class='foot-title'>已超过<span>{state.free_count}位</span>获得免费诊断</p>
+            <div class='input-item'>
+                <span class='input-label'>姓名:</span>
+                < input type = "text" class='input-form'
+                    oninput = {e => actions.nameInput({ value: e.target.value})}
+                    value = {state.name} />
+            </div>
+            <div class='input-item'>
+                <span class='input-label'>手机号:</span>
+                < input type="text" class='input-form'
+                    oninput={e => actions.phoneInput({ value: e.target.value })}
+                    value={state.phone} />
+            </div>
+            <div class='input-item'>
+                <span class='input-label'>验证码:</span>
+                < input type="text" class='input-form'
+                    oninput={e => actions.codeInput({ value: e.target.value })}
+                    value={state.code} />
+                <button class='input-btn'>获取验证码</button>
+            </div>
+            <button class='footer-btn'>获取更多专家免费诊断</button>
+        </footer>
     </div>
 );
 app(state, actions, view, document.body);
--- src/pages/index/js/module/list.js
+++ src/pages/index/js/module/list.js
@@ -1,48 +1,23 @@
-import { h, app } from 'hyperapp';
-import icon1 from 'Static/images/icon1.jpg';
-import icon2 from 'Static/images/icon2.jpg';
-import icon3 from 'Static/images/icon3.jpg';
-import code1 from 'Static/images/code1.png';
-import code2 from 'Static/images/code2.png';
-import code3 from 'Static/images/code3.png';
+import { h } from 'hyperapp';
 
-export const list = app({
-    state: {
-        listData: [
-            {
-                name: '往昔',
-                count: 1892,
-                praise: '100%',
-                desc: '12年操盘经验，顺应趋势，主攻中短线投资',
-                event: 'diagnosis',
-                icon: icon1,
-                qrcode: code1
-            },
-            {
-                name: '清水',
-                count: 2159,
-                praise: '99%',
-                desc: '有多年操盘有丰富实战技术，着重与量化寻找短线个股',
-                event: 'diagnosis',
-                icon: icon2,
-                qrcode: code2
-            },
-            {
-                name: '季璐',
-                count: 1362,
-                praise: '99%',
-                desc: '连续斩获多个涨停板，投资网红，实操大牛',
-                event: 'diagnosis',
-                icon: icon3,
-                qrcode: code3
-            }
-        ]
-    },
+export const List = ({ arr, event }) => arr.map(
+    item => (
+        <li class='list-item'>
+            <div class='main-box'>
+                <img class='item-icon' src={item.icon} />
+                <div class='base-info'>
+                    <h3>{item.name}</h3>
+                    <p class='base-info-data'>
+                        <span>解答:  {item.count}次</span>
+                        <span>好评:  {item.praise}</span>
+                    </p>
+                </div>
+                <button class='item-btn' onclick={() => event(item)}>免费诊断</button>
+            </div>
+            <p class='item-desc'>{item.desc}</p>
+        </li>
+    )
+);
 
-    view(state, actions) {
-        return (
-            <header>{state.caption}</header>
-        );
-    },
-});
+
 
